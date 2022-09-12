2d3d9635ab03550407271116b489023230df8508
Sven <112931469+Twilight2006@users.noreply.github.com>
2022-09-12 13:22:23 +0200

    Add files via upload

--- feestlunch.py
+++ feestlunch.py
@@ -1,7 +1,24 @@
-print (17 * 0.39)
+print('De feestlunch kost je bij de bakker 10.69 voor de 17 croissantjes en de 2 stokbroden als de 3 kortingsbonnen nog geldig zijn!')
 
-print (6.63 + 2 * 2.78)
+cro = 17
+prijs = 0.39
+sto = 2
+prijs2 = 2.78
+korting = 0.50
+aantal = 3
 
-print (3 * 0.50)
+totaal = cro * prijs
 
-print (12.19 - 1.5)
+print(totaal)
+
+totaal2 = sto * prijs2
+
+print(totaal2)
+
+totaal3 = korting * aantal
+
+print(totaal3)
+
+totaalprijs = totaal + totaal2 - totaal3
+
+print(totaalprijs)
--- speelyhal.py
+++ speelyhal.py
@@ -1,3 +1,5 @@
+print('Dit geweldige dagje-uit met 4 mensen in de Speelhal met 45 minuten VR kost je maar 43.12 euro')
+
 aantal = 4
 price = 7.45
 tijd = 45 
