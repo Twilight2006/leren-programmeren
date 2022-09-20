b0f4ffd6171091ff14a71fe0512955f0942f06e8
Twilight <99072042@mydavinci.nl>
2022-09-20 11:12:37 +0200

    commit

---
+++ Module 2/deel 3/input.py
@@ -0,0 +1,14 @@
+A = input(' vul een getal in ') #2x hetzelfde getal doet raar: <built-in function max> is het grootste getal
+B = input(' vul een getal in ')
+
+if A > B:
+    max = A
+elif B > A:
+    max = B
+
+print(str(max) + ' is het grootste getal ')
+
+
+
+
+
