958abafff30295ea59aac31914155e5d12e9cfbd
Twilight <99072042@mydavinci.nl>
2022-09-20 11:32:56 +0200

    else statement

--- Module 2/deel 3/input.py
+++ Module 2/deel 3/input.py
@@ -1,5 +1,5 @@
-A = input(' vul een getal in ') #2x hetzelfde getal doet raar: <built-in function max> is het grootste getal
-B = input(' vul een getal in ')
+A = input(' vul een getal in: ') #2x hetzelfde getal doet raar: (<built-in function max> is het grootste getal)
+B = input(' vul een getal in: ')
 
 if A > B:
     max = A
@@ -7,6 +7,23 @@ elif B > A:
     max = B
 
 print(str(max) + ' is het grootste getal ')
+
+if A < B:
+    min = A
+elif B < A:
+    min = B
+
+print(str(min) + ' is het kleinste getal ')
+
+if A == B:
+    print(' A en B zijn gelijk ')
+else:
+    print(' A en B zijn niet gelijk ')
+
+
+
+
+
 
 
 
