c8541c380d70bf553779fca74db99cac774415bf
Twilight <99072042@mydavinci.nl>
2022-09-21 10:57:18 +0200

    commit

---
+++ Module 2/deel 3/kaas.py
@@ -0,0 +1,12 @@
+print(' hallo meneer/mevrouw, ik ben kaasbot en ik ga proberen te raden welke kaas u in uw hoofd hebt! ')
+print(' ik ga u een paar vragen stellen waarop u antwoord moet geven ')
+
+geel = input(' is de kaas geel? J/N: ')
+ 
+if geel
+    print(' ok ' )
+elif geel
+    print(' goed om te weten ')
+else:
+    print(' dat is geen mogelijkheid ')
+
