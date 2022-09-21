ceb8730cce883f93dfe93acaeed88ce23c320094
Twilight <99072042@mydavinci.nl>
2022-09-21 12:03:42 +0200

    say cheese!

--- Module 2/deel 3/kaas.py
+++ Module 2/deel 3/kaas.py
@@ -1,12 +1,46 @@
 print(' hallo meneer/mevrouw, ik ben kaasbot en ik ga proberen te raden welke kaas u in uw hoofd hebt! ')
 print(' ik ga u een paar vragen stellen waarop u antwoord moet geven ')
 
-geel = input(' is de kaas geel? J/N: ')
- 
-if geel
-    print(' ok ' )
-elif geel
-    print(' goed om te weten ')
+geel = input(' is de kaas geel of niet? J/N : ')
+
+if geel == 'nee':
+    schimmel = input(' heeft de kaas blauwe schimmel? : ')
+    if schimmel == 'ja':
+        korst = input(' heeft de kaas korst? : ')
+    if korst == 'nee':
+        print(' is uw kaas fourme d' 'ambert? ' )
+    elif korst == 'ja':
+            print('is uw kaas Blue de Rochbaron?')
+
+    elif schimmel == 'nee':
+        korst2 = input(' heeft de kaas korst? : ')
+    if korst2 == 'ja':
+        print('is uw kaas Camembert?')
+    elif korst2 == 'nee':
+            print('is uw kaas Mozzarella?')
+
+                 
+
+
+elif geel == 'ja':
+    gaten = input(' heeft de kaas gaten? : ')
+    if gaten == 'ja':
+        prijs = input(' is de kaas belagelijk duur? : ') 
+        if prijs == 'nee':
+            print('is uw kaas Leerdammer? ')
+        elif prijs == 'ja':
+                print('is uw kaas Emmenthaler? ')
+        
+    elif gaten == 'nee':
+            hard = input(' is de kaas belagelijk hard? : ')
+            if hard == 'ja':
+                print(' is uw kaas Parmigiano Reggiano? ')
+            elif hard == 'nee':
+                print(' is uw kaas Goudsekaas? ')
+
 else:
-    print(' dat is geen mogelijkheid ')
+    print(' error-error-error,kzzr dat is geen kzzzzrztt mogelijkheid kzzrzzt')
+            
+        
+      
 
