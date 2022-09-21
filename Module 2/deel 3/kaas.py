bae5f51550c0603bfc0c4efafe0485ea31ce022e
Twilight <99072042@mydavinci.nl>
2022-09-21 18:46:02 +0200

    say cheese!

--- Module 2/deel 3/kaas.py
+++ Module 2/deel 3/kaas.py
@@ -7,49 +7,43 @@ if geel == 'nee':
     schimmel = input(' heeft de kaas blauwe schimmel? : ')
     if schimmel == 'ja':
         korst = input(' heeft de kaas korst? : ')
-    if korst == 'nee':
-        aw1 = input(' is uw kaas fourme d' 'ambert? : ' )
-        if aw1 == 'ja':
+        if korst == 'nee':
+            aw1 = input(' is uw kaas fourme d' 'ambert? : ' )
+            if aw1 == 'ja':
                 print(' dankuwel voor het spelen ^_^ ')
-        elif aw1 == 'nee':
+            elif aw1 == 'nee':
                     print('oeps, sorry. herstart mijn programma om het opnieuw te proberen, krzzzt! ')
-
-    
-    elif korst == 'ja':
-            aw2 = input('is uw kaas Blue de Rochbaron? :')
-            if aw2 == 'ja':
-                print(' dankuwel voor het spelen ^_^ ')
-            elif aw2 == 'nee':
-                    print('oeps, sorry. herstart mijn programma om het opnieuw te proberen, krzzzt! ')
+   
+            elif korst == 'ja':
+                    aw2 = input(' is uw kaas Blue de Rochbaron? :')
+                    if aw2 == 'ja':
+                        print(' dankuwel voor het spelen ^_^ ')
+                    elif aw2 == 'nee':
+                        print('oeps, sorry. herstart mijn programma om het opnieuw te proberen, krzzzt! ')
 
     elif schimmel == 'nee':
         korst2 = input(' heeft de kaas korst? : ')
-    if korst2 == 'ja':
-        aw3 = input('is uw kaas Camembert? :')
-
-    if aw3 == 'ja':
+        if korst2 == 'ja':
+            aw3 = input(' is uw kaas Camembert? : ')
+            if aw3 == 'ja':
                 print(' dankuwel voor het spelen ^_^ ')
-    elif aw3 == 'nee':
+            elif aw3 == 'nee':
                 print('oeps, sorry. herstart mijn programma om het opnieuw te proberen, krzzzt! ')
         
-
-
-    elif korst2 == 'nee':
-            aw4 = input('is uw kaas Mozzarella? :')
-            if aw4 == 'ja':
-                print(' dankuwel voor het spelen ^_^ ')
-            elif aw4 == 'nee':
+        elif korst2 == 'nee':
+                aw4 = input(' is uw kaas Mozzarella? :')
+                if aw4 == 'ja':
+                    print(' dankuwel voor het spelen ^_^ ')
+                elif aw4 == 'nee':
                     print('oeps, sorry. herstart mijn programma om het opnieuw te proberen, krzzzt! ')
 
                  
-
-
 elif geel == 'ja':
     gaten = input(' heeft de kaas gaten? : ')
     if gaten == 'ja':
         prijs = input(' is de kaas belagelijk duur? : ') 
         if prijs == 'nee':
-            aw5 = input('is uw kaas Leerdammer? : ')
+            aw5 = input(' is uw kaas Leerdammer? : ')
             if aw5 == 'ja':
                 print(' dankuwel voor het spelen ^_^ ')
             elif aw5 == 'nee':
@@ -57,7 +51,7 @@ elif geel == 'ja':
             
 
         elif prijs == 'ja':
-                aw6 = input('is uw kaas Emmenthaler? :')
+                aw6 = input(' is uw kaas Emmenthaler? :')
                 if aw6 == 'ja':
                     print(' dankuwel voor het spelen ^_^ ')
                 elif aw6 == 'nee':
@@ -67,9 +61,9 @@ elif geel == 'ja':
             hard = input(' is de kaas belagelijk hard? : ')
             if hard == 'ja':
                 aw7 = input(' is uw kaas Parmigiano Reggiano? : ')
-            if aw7 == 'ja':
-                print(' dankuwel voor het spelen ^_^ ')
-            elif aw7 == 'nee':
+                if aw7 == 'ja':
+                    print(' dankuwel voor het spelen ^_^ ')
+                elif aw7 == 'nee':
                     print('oeps, sorry. herstart mijn programma om het opnieuw te proberen, krzzzt! ')
 
             elif hard == 'nee':
@@ -79,9 +73,8 @@ elif geel == 'ja':
                 elif aw8 == 'nee':
                     print('oeps, sorry. herstart mijn programma om het opnieuw te proberen, krzzzt! ')
 
-
-    else:
-        print(' error-error-error,kzzr dat is geen mogelijkheid kzzzzt')
+else:
+    print(' error-error-error,kzzr dat is geen mogelijkheid kzzzzt')
             
         
       
