11f924f9ac92d4315468ae3ac53cbef74791360c
Twilight <99072042@mydavinci.nl>
2022-10-04 13:49:25 +0200

    4-10-22

---
+++ Module 2/deel 4/STP.py
@@ -0,0 +1,10 @@
+gastheer = input(' Wat is dw naam van de gastheer? ')
+gasten = 6
+drank = True
+chips = True
+
+if gastheer != 'rudi' or gasten > 4 and gasten < 13 and drank == True or chips == True:
+    print(' Start the party! ')
+
+else:
+    print(' No party.... ')
---
+++ Module 2/deel 4/bugs.py
@@ -0,0 +1,46 @@
+import random
+
+name = input('Wat is jouw naam? ')
+print('Hallo', name)
+
+favoriteSeason = input('Wat is jouw favorite seizoen '+ name + '? A) Lente, B) Zomer, C) Herfst of D) Winter ')
+answer = favoriteSeason.lower
+
+if answer == 'a':
+    print("Ik hou ook van de lente!")
+
+elif answer == 'b':
+    print("De zomer is voor mij te warm.")
+
+elif answer == 'c':
+    print("Mooi he, al die blaadjes die dan van de boom vallen.")
+
+elif answer == 'd':
+    print("Is de winter niet erg koud?")
+
+else:
+    print("Die ken ik niet...")
+
+favoriteColor = input('En wat is je favoriete kleur? ') 
+trueOrFalse = str(random.randint(0,1))
+if True:
+    print('Ik vind dat ook een mooie kleur!')
+
+elif False:
+    print('TBH, ik hou niet zo van {}...'.format(favoritecolor))
+
+num1 = random.randint(1,10)
+num2 = random.randint(5,15)
+
+try:
+    number = input( 'En weet jij wat ' + str(num1) + '+' + str(num2)  + ' is? ')
+
+except:
+    print('Nee dat klopt niet {}'.format(name)) 
+
+if int(number == num1-num2):
+    print('Dat is juist')
+
+else:
+    print('Dat is geen nummer!')
+
