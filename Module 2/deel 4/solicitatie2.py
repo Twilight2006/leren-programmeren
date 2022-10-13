e6cf256c020983d3eae9f36f985da63e89b62a14
Twilight <99072042@mydavinci.nl>
2022-10-13 09:11:56 +0200

    working

--- Module 2/deel 4/solicitatie2.py
+++ Module 2/deel 4/solicitatie2.py
@@ -7,12 +7,12 @@ try:
         raise NameError('U mag niet dezelfde naam hebben als de eigenaar! ')
     print('Hallo ' + name )
     gender = input('Bent u een man of een vrouw? ')
-    if gender == 'man':
+    if gender == 'man' or 'Man' :
             diploma = input('Heeft u een MBO-4 diploma? ja/nee : ')
             rijbewijs = input('heeft u een geldig vrachtwagen rijbewijs? ja/nee : ')
             hoed = input('heeft u een hoge hoed? ja/nee :')
             snor = input('heeft u een snor? ja/nee : ')
-            if snor == 'ja':
+            if snor == 'ja' or 'Ja':
                 jsnor = input('Is uw snor langer dan 10 cm? ja/nee : ')
                 lengte = input('Bent u langer dan 150 cm? ')
                 gewicht = input('Bent u zwaarder dan 80 kg? : ')
@@ -20,19 +20,28 @@ try:
                 dd = int(input('Hoeveel jaar ervaring heeft u met dieren-dressuur? :'))
                 acrobatiek = int(input('Hoeveel jaar ervaring heeft u met acrobatiek? : '))
                 jongleren = int(input('Hoeveel jaar ervaring heeft u met jongleren? :'))
-            elif snor =='nee':
+            elif snor =='nee' or 'Nee':
                 print('Sorry meneer, u moet een snor hebben.')
     
 
-    elif gender == 'nee':
-        diploma = input('Heeft u een MBO-4 diploma?: ')
-        rijbewijs = input('Heeft u een geldig vrachtwagen rijbewijs? ja/nee : ')
-        hoed = input('Heeft u een hoge hoed? ja/nee :')
-        haar = input('Heeft u lang rood haar? ja/nee :')
-        if haar == 'ja':
-            print
+    elif gender == 'vrouw' or 'Vrouw' :
+            diploma = input('Heeft u een MBO-4 diploma?: ')
+            rijbewijs = input('Heeft u een geldig vrachtwagen rijbewijs? ja/nee : ')
+            hoed = input('Heeft u een hoge hoed? ja/nee :')
+            haar = input('Heeft u lang rood haar? ja/nee :')
+            if haar == 'ja' or 'Ja':
+                haar1 = input('Is uw haar langer dan 20 cm? ja/nee : ')
+                lengte = input('bent u langer dan 150 cm? ')
+                gewicht = input('bent u zwaarder dan 80 kg? : ')
+                certificaat = input('heeft u een certififaat voor Overleven met gevaarlijk personeel? ja/nee ')
+                dd = int(input('Hoeveel jaar ervaring heeft u met dieren-dressuur? :'))
+                acrobatiek = int(input('hoeveel jaar ervaring heeft u met acrobatiek? : '))
+                jongleren = int(input('hoeveel jaar ervaring heeft u met jongleren? :' ))
+            elif haar == 'nee' or 'Nee':
+                print('Sorry mevrouw, u moet lang rood haar hebben')
 
-
+                            
+                            
 except NameError as e:
     print('U mag niet dezelfde naam hebben als de eigenaar!')
 
@@ -41,6 +50,30 @@ except ValueError as e:
 
 finally:
     print('Dankuwel voor uw solicitatie! ')
+
+if gender == 'man':
+    if diploma == 'ja':
+            if rijbewijs == 'ja':
+                    if hoed == 'ja':
+                        if snor == 'ja':
+                            if jsnor == 'ja':
+                                if lengte == 'ja':
+                                    if gewicht == 'ja':
+                                        if certificaat == 'ja':
+                                             print(' u bent aangenomen! ')
+
+elif gender == 'vrouw':
+    if diploma == 'ja':
+        if rijbewijs == 'ja':
+                if hoed == 'ja':
+                    if snor == 'ja':
+                        if jsnor == 'ja':
+                            if lengte == 'ja':
+                                if gewicht == 'ja':
+                                    if certificaat == 'ja':
+                                            print(' u bent aangenomen! ')
+
+
 
 
 
