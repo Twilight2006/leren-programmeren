823edcd200272472d8f709354a11e4ce6cda4885
Twilight <99072042@mydavinci.nl>
2022-10-13 15:56:30 +0200

    Almost halfqay done :)

--- Module 2/Proef A/RotK.py
+++ Module 2/Proef A/RotK.py
@@ -1,10 +1,10 @@
-
+print('Welkom bij: Return of the Kongs!')
 name = input('Vul uw naam in AUB: ')
 
 print('Het is een stille nacht......')
 print('Gewoon een normale nacht als altijd, voor ' + name +'.')
 print('De sterren schijnen, de krekels tjirpen, Je kat ligt in haar mand,')
-print('En jij licht wakker, op je telefoon.')
+print('En jij ligt wakker, op je telefoon.')
 print('Dat is niets nieuws voor ' + name + '.')
 print('Meestal kijk je in bed nog tot ongeveer 1 uur op je telefoon tot je echt gaat slapen.')
 print('Maar vandaag was anders.')
@@ -17,12 +17,91 @@ if antwoord == 'a':
     print('---Einde---')
 elif antwoord =='b':
     print('Je gaat naar de hoek van je kamer, waar je denkt dat er iets naar je aan het kijken is.')
-    print('Als je dichterbij komt, voel je een steek in je zij.')
-    print('Je wordt duizelig en begint stemmen in je hoofd te horen...')
+    print('Als je dichterbij komt, voel je een steek in je zij. Je begint stemmen in je hoofd te horen.')
     print('?: ooh ooh ah?')
-    print('?')
     print('Wat is dit?')
     print('Je voelt jezelf wegdrijven in een diepe slaap...')
-else:
-    print('Dat kan niet. Restarting program...(herstart het programma)')
+    print('Je wordt wakker na wat voelt als een eeuwigheid.')
+    print('Je voelt je een beetje duizelig, maar het is nog wel te doen.')
+    print('Je doet je ogen open en strekt.')
+    print('Je bent niet in je eigen kamer!')
+    print('Waar ben ik?')
+    print('Ben ik geontvoerd? is dit een droom?')
+    print('Je slaat jezelf.')
+    print('wacht, wat was dat? ')
+    print('Toen jejezelf raakte, voelde je een raar soort haar...')
+    print('Je kijkt naar je hand.')
+    print('Je kan je ogen niet geloven. Nee, je WILT je ogen niet geloven.')
+    print('BEN IK EEN AAP GEWORDEN? ')
+    print('Riep ' + name + ' zo hard als die kon.')
+    print('Daarna opent er een deur....')
+    print('Wacht....')
+    print('Is dat.....')
+    print('DONKEY KONG?')
+    print('Ja hoor, er komt een Donkey Kong uit de deur.')
+    print('ooh ooh ha hahaa ooh? ' + '(Hallo, ' + name  + '!)')
+    print('Waarom kan je hem verstaan?')
+    print('ooooohhooooooohoooohoooooi!' '(Ik heb je meegenomen naar onze wereld omdat je ons moet helpen!)')
+    print('ooh ooh ah ah' + '(Onze schat is gestolen, wil je ons alsjeblieft helpen ' + ', ' +  name + '?)')
+    help = input('Wat zeg je?: A:nee / B:ja  ')
+    if help == 'a':
+        print('Waarom springt Donkey Kong op je af? Waarom... Ziet ie er boos uit? ')
+        print('---Einde---')
+    elif help == 'b':
+        print('oooh hooo hooi' '(Dankje, ' +  name + '.' + 'Zie je die tempel daar? Daar is onze schat. Iemand heeft onze schat daarheen gebracht.)')
+        print('OK, leid me maar de weg, Donkey Kong!')
+        print('hooohooohaha' + '(Wacht, ' +  name + ' klinkt niet als een aap, er moet een mooie naam achter! Wat vind je van ' + name + ' kong?)')
+        kong = input('Wat zeg je? A:nee / B:ja : ')
+        if kong == 'a':
+            print('Nee, dat wil ik niet. Dat vindt ik geen leuke naam.')
+            print('Dat vindt Donkey Kong niet leuk.')
+            print('ooooh ooh ooha ' + ' (Je maakt traditie kapot. Weg met jou!)')
+            print('---GAME OVER---')
+        elif kong == 'b':
+            print('Ja hoor, noem me maar ' + name + ' kong ')
+            print('Terwijl jullie 2 naar de tempel lopen, begin je een beetje te twijfelen.')
+            print('Maar je gaat nu niets meer terug. Je gaat ervoor.')
+            print('Je komt de tempel in.')
+            print('Donkey Kong zwaait je na en loopt terug naar het huis waar jullie vandaan komen.')
+            print('Je loopt de trappen af.')
+            print('[34 minuten later]')
+            print('Sheesh, dit zijn wel heel veel trappen.')
+            print('Je blijft doorlopen, totdat je onderaan komt.')
+            print('Na nog een paar minuten, kom je beneden.')
+            print('Je ziet een grote hal, met twee kanten.')
+            print('Oh nee.')
+            print('Je moet kiezen tussen twee kanten. welke kant kies je?')
+            side = input('Kies wijs: links of Rechts?: A-links / B-rechts')
+            if side == 'a':
+                print('Je gaat de linkse kamer in')
+                print('Hij is nogal donker, maar je kan nog wel dingen zien.')
+                print('Ooooooh boy')
+                print('Nu begint het.')
+                print('Je ziet een gigantische dobbelsteen, met een bord daarnaast.')
+                print('{Oh, thzou who will be poor enough to enter zhis cursed tzemple,')
+                print('Thzou will not pass without a zbrain.')
+                print('Thzou will have to choose a anwser based on what i ask Tzhou,')
+                print('Good ZLuck poor Zhoul.}')
+                print('Oh god, dit komt nooit goed.')
+                print('Opeens hoor je uit een gigantische luidspreker in je oor roepen..')
+                awnser = input('Oh THZOU WHO ENTERED TZHIS CURSED LAND, WHAT IZT 5+5x2:3+1? Round UP^')
+                if awnser == '8':
+                    print('DONE')
+                else:
+                    print('YOU WILL DIE')
 
+               
+
+
+            elif side == 'b':
+                print()
+                
+            else:
+                print('Dat is geen mogelijkheid, Restarting program...(herstart het programma)')
+
+    else:
+        print('Dat is geen mogelijkheid, Restarting program...(herstart het programma)')
+    
+    
+else:
+    print('Dat is geen mogelijkheid, Restarting program...(herstart het programma)')
