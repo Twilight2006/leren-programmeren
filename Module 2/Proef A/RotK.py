315e72fe5c77a7510cd7846ab56dbac1c8014358
Twilight <99072042@mydavinci.nl>
2022-10-13 12:06:57 +0200

    saved

---
+++ Module 2/Proef A/RotK.py
@@ -0,0 +1,28 @@
+
+name = input('Vul uw naam in AUB: ')
+
+print('Het is een stille nacht......')
+print('Gewoon een normale nacht als altijd, voor ' + name +'.')
+print('De sterren schijnen, de krekels tjirpen, Je kat ligt in haar mand,')
+print('En jij licht wakker, op je telefoon.')
+print('Dat is niets nieuws voor ' + name + '.')
+print('Meestal kijk je in bed nog tot ongeveer 1 uur op je telefoon tot je echt gaat slapen.')
+print('Maar vandaag was anders.')
+print('Je hebt het gevoel dat er iets naar je kijkt.')
+print('In het donker....')
+print('Wat ge je doen?')
+antwoord = input('A: Blijf liggen / B: Ga kijken ' ': ')
+if antwoord == 'a':
+    print('Je blijft in je bed liggen, en gaat gewoon door zoals normaal.')
+    print('---Einde---')
+elif antwoord =='b':
+    print('Je gaat naar de hoek van je kamer, waar je denkt dat er iets naar je aan het kijken is.')
+    print('Als je dichterbij komt, voel je een steek in je zij.')
+    print('Je wordt duizelig en begint stemmen in je hoofd te horen...')
+    print('?: ooh ooh ah?')
+    print('?')
+    print('Wat is dit?')
+    print('Je voelt jezelf wegdrijven in een diepe slaap...')
+else:
+    print('Dat kan niet. Restarting program...(herstart het programma)')
+
