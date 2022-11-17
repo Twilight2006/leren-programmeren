b4dc85259df37863f1a0cef9dc9ec9d646291707
Twilight <99072042@mydavinci.nl>
2022-11-17 11:19:52 +0100

    done

---
+++ Module 3/deel 02/wontshutup.py
@@ -0,0 +1,10 @@
+for i in range(100000000000000000000000):
+    quit = input('?: ')
+    if quit == 'quit':
+        print('Dankuwel!')
+        break
+
+    while quit != 'quit':
+        break
+
+print(i , 'keer herhaald :)' )
