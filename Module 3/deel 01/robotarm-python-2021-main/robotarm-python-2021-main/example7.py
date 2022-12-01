f26f13a6c2df9e62b009466fb85464cc22ec25f9
Twilight <99072042@mydavinci.nl>
2022-12-01 10:23:05 +0100

    nope

--- Module 3/deel 01/robotarm-python-2021-main/robotarm-python-2021-main/example7.py
+++ Module 3/deel 01/robotarm-python-2021-main/robotarm-python-2021-main/example7.py
@@ -1,9 +1,13 @@
 from RobotArm import RobotArm
 
-robotArm = RobotArm('exercise 1')
+robotArm = RobotArm('exercise 7')
 
-# Jouw python instructies zet je vanaf hier:
+for i in range(5):
+    for i in range(2):
+        robotArm.moveRight()
+        robotArm.grab()
+        robotArm.moveLeft()
+        robotArm.drop()
 
 
-# Na jouw code wachten tot het sluiten van de window:
 robotArm.wait()
---
+++ Projecten/robotarm/example.py
@@ -0,0 +1,8 @@
+from RobotArm import RobotArm
+
+robotArm = RobotArm('exercise 7')
+
+robotArm.moveRight
+
+
+robotArm.wait()
