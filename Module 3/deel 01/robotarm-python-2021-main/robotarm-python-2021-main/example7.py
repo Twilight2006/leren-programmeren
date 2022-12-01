2cf69994ef4d89fd3db229217f94e450786a5ab9
Twilight <99072042@mydavinci.nl>
2022-12-01 11:04:35 +0100

    ()

--- Module 3/deel 01/robotarm-python-2021-main/robotarm-python-2021-main/example7.py
+++ Module 3/deel 01/robotarm-python-2021-main/robotarm-python-2021-main/example7.py
@@ -1,13 +1,9 @@
 from RobotArm import RobotArm
-
 robotArm = RobotArm('exercise 7')
-
-for i in range(5):
-    for i in range(2):
-        robotArm.moveRight()
-        robotArm.grab()
-        robotArm.moveLeft()
-        robotArm.drop()
-
+for i in range(6):
+    robotArm.moveRight()
+    robotArm.grab()
+    robotArm.moveLeft()
+    robotArm.drop()
 
 robotArm.wait()
