f6aa711c90fd0a69af922cf1cad1e54982657c62
Twilight <99072042@mydavinci.nl>
2023-01-19 09:58:11 +0100

    Deck afgemaakt. begin aan fruitmand

--- Module 4/deel 1/KaartenDeck.py
+++ Module 4/deel 1/KaartenDeck.py
@@ -1,8 +1,8 @@
 import random
 nummers = [2,3,4,5,6,7,8,9,10, "Boer", "Vrouw", "Heer", "Aas"]
-joker = 2
+joker = "joker"
 kleuren = ["Harten", "Klaveren", "Schoppen", "Ruiten"]
-deck = []
+deck = [joker,joker]
 
 for i in range(len(kleuren)):
     for j in range(len(nummers)):
@@ -13,7 +13,7 @@ for i in range (1,8):
     print(f"Kaart {i}; {deck.pop(0)}")
 
 print("")
-print(f"deck (47 kaarten): {deck}")
+print(f"deck {len(deck)}: {deck}")
 
 
 
---
+++ Module 4/deel 2/Fruitmand/opdracht02.py
@@ -0,0 +1,5 @@
+from fruitmand import fruitmand
+
+print(fruitmand[0])
+
+
